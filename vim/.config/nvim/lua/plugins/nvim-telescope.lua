c18b430f3b2fb27654f6530024763a393eed5e37
Jovan Frandika <58182201+jovanfrandika@users.noreply.github.com>
2024-02-26 14:24:47 +0700

    chore: Add new action to telescope file browser and update Brewfile

--- vim/.config/nvim/lua/plugins/nvim-telescope.lua
+++ vim/.config/nvim/lua/plugins/nvim-telescope.lua
@@ -1,13 +1,12 @@
-
 return {
   "nvim-telescope/telescope.nvim",
   cmd = "Telescope",
   dependencies = {
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-      "folke/noice.nvim",
-      "nvim-telescope/telescope-file-browser.nvim"
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "folke/noice.nvim",
+    "nvim-telescope/telescope-file-browser.nvim",
   },
-  opts = function() 
+  opts = function()
     local actions = require("telescope.actions")
     local fb_actions = require("telescope").extensions.file_browser.actions
 
@@ -16,38 +15,38 @@ return {
         theme = "center",
         sorting_strategy = "ascending",
         layout_config = {
-            prompt_position = "top",
+          prompt_position = "top",
         },
         file_ignore_patterns = {
-            "^.git/*",
-            "node_modules/*",
-            ".mypy_cache/*",
+          "^.git/*",
+          "node_modules/*",
+          ".mypy_cache/*",
         },
         mappings = {
-            i = {
-                ["<Tab>"] = actions.move_selection_next,
-                ["<S-Tab>"] = actions.move_selection_previous,
-                ["<C-n>"] = actions.toggle_selection + actions.move_selection_worse,
-                ["<C-p>"] = actions.toggle_selection + actions.move_selection_better,
-            },
+          i = {
+            ["<Tab>"] = actions.move_selection_next,
+            ["<S-Tab>"] = actions.move_selection_previous,
+            ["<C-n>"] = actions.toggle_selection + actions.move_selection_worse,
+            ["<C-p>"] = actions.toggle_selection + actions.move_selection_better,
+          },
         },
       },
       pickers = {
         buffers = {
-            mappings = {
-                i = {
-                    ["<S-BS>"] = actions.delete_buffer,
-                },
+          mappings = {
+            i = {
+              ["<S-BS>"] = actions.delete_buffer,
             },
+          },
         },
         lsp_references = {
-            show_line = false,
+          show_line = false,
         },
         find_files = {
-            hidden = true,
+          hidden = true,
         },
         live_grep = {
-            additional_args = { "--hidden" },
+          additional_args = { "--hidden" },
         },
       },
       extensions = {
@@ -69,22 +68,24 @@ return {
               ["h"] = fb_actions.goto_parent_dir,
               ["l"] = actions.select_default,
               ["H"] = fb_actions.toggle_hidden,
+              ["N"] = fb_actions.create,
             },
           },
         },
-      }
+      },
     }
   end,
   config = function(_, opts)
-      local telescope = require("telescope")
-      telescope.setup(opts)
-      telescope.load_extension("fzf")
-      telescope.load_extension("noice")
-      telescope.load_extension("file_browser")
+    local telescope = require("telescope")
+    telescope.setup(opts)
+    telescope.load_extension("fzf")
+    telescope.load_extension("noice")
+    telescope.load_extension("file_browser")
   end,
   keys = {
-      { "<leader>ff", "<CMD>Telescope find_files<CR>", noremap = true },
-      { "<leader>fg", "<CMD>Telescope live_grep<CR>", noremap = true },
-      { "<leader>fp", "<CMD>Telescope project<CR>", noremap = true },
+    { "<leader>ff", "<CMD>Telescope find_files<CR>", noremap = true },
+    { "<leader>fg", "<CMD>Telescope live_grep<CR>", noremap = true },
+    { "<leader>fp", "<CMD>Telescope project<CR>", noremap = true },
   },
 }
+
